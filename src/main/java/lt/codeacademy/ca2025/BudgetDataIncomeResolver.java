dcbf6f19786331d4b7d0caf04cb059fc600187ff
Pavel Vrublevskij <pavel.vrublevskij@nortal.com>
2025-02-18 20:50:06 +0200

    create self check exception and catch in other classes

--- src/main/java/lt/codeacademy/ca2025/BudgetDataIncomeResolver.java
+++ src/main/java/lt/codeacademy/ca2025/BudgetDataIncomeResolver.java
@@ -2,7 +2,9 @@ package lt.codeacademy.ca2025;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import lt.codeacademy.ca2025.exception.AmountLessOrEqualZeroException;
 
 public class BudgetDataIncomeResolver {
 
@@ -13,21 +15,29 @@ public class BudgetDataIncomeResolver {
 		this.budgetService = budgetService;
 	}
 
-	public void resolve() {
-		System.out.println("Please enter your income record.");
-		System.out.print("Amount: ");
-		final BigDecimal amount = sc.nextBigDecimal();
-		sc.nextLine();
-		System.out.print("Description: ");
-		final String description = sc.nextLine();
-		System.out.print("Is it a bank transfer? (true/false): ");
-		final boolean isBankTransfer = sc.nextBoolean();
-		System.out.print("Please enter other optional information: ");
-		final String optionalInfo = sc.nextLine();
+	public void resolve() throws AmountLessOrEqualZeroException, InputMismatchException {
+		try {
+			System.out.println("Please enter your income record.");
+			System.out.print("Amount: ");
+			final BigDecimal amount = sc.nextBigDecimal();
+			sc.nextLine();
+			if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+				throw new AmountLessOrEqualZeroException();
+			}
+			System.out.print("Category: ");
+			final String category = sc.nextLine();
+			System.out.print("Is it a bank transfer? (true/false): ");
+			final boolean isBankTransfer = sc.nextBoolean();
+			System.out.print("Please enter other optional information: ");
+			final String optionalInfo = sc.nextLine();
 
-		final IncomeRecord incomeRecord = new IncomeRecord(
-				amount, description, LocalDateTime.now(), isBankTransfer, optionalInfo);
+			final IncomeRecord incomeRecord = new IncomeRecord(
+					amount, category, LocalDateTime.now(), isBankTransfer, optionalInfo);
 
-		budgetService.setIncomeRecord(incomeRecord);
+			budgetService.setIncomeRecord(incomeRecord);
+		} catch (InputMismatchException e) {
+			sc.nextLine();
+			throw new InputMismatchException();
+		}
 	}
 }
--- src/main/java/lt/codeacademy/ca2025/BudgetManager.java
+++ src/main/java/lt/codeacademy/ca2025/BudgetManager.java
@@ -2,7 +2,9 @@ package lt.codeacademy.ca2025;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.InputMismatchException;
 import java.util.List;
+import lt.codeacademy.ca2025.exception.AmountLessOrEqualZeroException;
 
 public class BudgetManager {
 	private final BudgetService budgetService = new BudgetService();
@@ -11,7 +13,17 @@ public class BudgetManager {
 	public void run() {
 		System.out.println("Hello My Budget! This is Demo Version!");
 
-		budgetDataIncomeResolver.resolve();
+		while (true) {
+			try {
+				budgetDataIncomeResolver.resolve();
+				break;
+			} catch (AmountLessOrEqualZeroException e) {
+				System.out.println(e.getMessage().concat(" Please try again."));
+			} catch (InputMismatchException e) {
+				System.out.println("Entered last value is not correct. Please try again.");
+			}
+		}
+
 		final ExpenseRecord expenseRecord = new ExpenseRecord(
 				BigDecimal.valueOf(100), "Food", LocalDateTime.now(), PaymentMethodType.CARD, new BankCard("Revolut", "1234"));
 		budgetService.setExpenseRecords(expenseRecord);
